85a9de644be20f1109531339ada8fe809c2cf53e
mahdi <alfred.mahdi@gmail.com>
2026-05-20 19:03:37 +0330

    Remove old workflows to clean up

--- Library/TeamTalkLib/avstream/OboeWrapper.h
+++ Library/TeamTalkLib/avstream/OboeWrapper.h
@@ -1,52 +1,86 @@
 /*
- * OboeWrapper.h
+ * Copyright (c) 2005-2018, BearWare.dk
+ *
+ * Contact Information:
+ *
+ * Bjoern D. Rasmussen
+ * Kirketoften 5
+ * DK-8260 Viby J
+ * Denmark
+ * Email: contact@bearware.dk
+ * Phone: +45 20 20 54 59
+ * Web: http://www.bearware.dk
+ *
+ * This source code is part of the TeamTalk SDK owned by
+ * BearWare.dk. Use of this file, or its compiled unit, requires a
+ * TeamTalk SDK License Key issued by BearWare.dk.
+ *
+ * The TeamTalk SDK License Agreement along with its Terms and
+ * Conditions are outlined in the file License.txt included with the
+ * TeamTalk SDK distribution.
+ *
  */
+
 #ifndef OBOEWRAPPER_H
 #define OBOEWRAPPER_H
 
 #include "SoundSystemBase.h"
 #include <oboe/Oboe.h>
+
+#include <map>
 #include <mutex>
+#include <set>
 #include <vector>
 
 namespace soundsystem {
 
-    struct OboeInputStreamer : InputStreamer, public oboe::AudioStreamDataCallback {
+    struct OboeInputStreamer : InputStreamer, public oboe::AudioStreamCallback
+    {
         std::shared_ptr<oboe::AudioStream> stream;
         std::recursive_mutex mutex;
+
         std::vector<short> buffer;
+        int fifo_size = 0;
 
         OboeInputStreamer(StreamCapture* r, int sg, int fs, int sr, int chs, SoundAPI sndsys, int devid)
             : InputStreamer(r, sg, fs, sr, chs, sndsys, devid) { }
 
         oboe::DataCallbackResult onAudioReady(oboe::AudioStream *oboeStream, void *audioData, int32_t numFrames) override;
+        void onErrorAfterClose(oboe::AudioStream *oboeStream, oboe::Result error) override;
     };
 
-    struct OboeOutputStreamer : OutputStreamer, public oboe::AudioStreamDataCallback {
+    struct OboeOutputStreamer : OutputStreamer, public oboe::AudioStreamCallback
+    {
         std::shared_ptr<oboe::AudioStream> stream;
         std::recursive_mutex mutex;
+
         std::vector<short> buffer;
+        int fifo_size = 0;
 
         OboeOutputStreamer(StreamPlayer* p, int sg, int fs, int sr, int chs, SoundAPI sndsys, int devid)
             : OutputStreamer(p, sg, fs, sr, chs, sndsys, devid) { }
 
         oboe::DataCallbackResult onAudioReady(oboe::AudioStream *oboeStream, void *audioData, int32_t numFrames) override;
+        void onErrorAfterClose(oboe::AudioStream *oboeStream, oboe::Result error) override;
     };
 
-    struct OboeSoundGroup : SoundGroup {
+    struct OboeSoundGroup : SoundGroup
+    {
         std::recursive_mutex mutex;
         OboeSoundGroup() = default;
     };
 
     typedef SoundSystemBase< OboeSoundGroup, OboeInputStreamer, OboeOutputStreamer, DuplexStreamer > SSB;
-
-    class OboeWrapper : public SSB {
+    
+    class OboeWrapper : public SSB
+    {
     private:
         OboeWrapper();
-        OboeWrapper(const OboeWrapper&) = delete;
-        const OboeWrapper& operator=(const OboeWrapper&) = delete;
+        OboeWrapper(const OboeWrapper& aud);
+        const OboeWrapper& operator = (const OboeWrapper& aud);
 
     protected:
+
         bool Init() override;
         void Close() override;
         void FillDevices(sounddevices_t& sounddevs) override;
@@ -54,39 +88,56 @@ namespace soundsystem {
         soundgroup_t NewSoundGroup() override;
         void RemoveSoundGroup(soundgroup_t sndgrp) override;
 
-        // Input
-        inputstreamer_t NewStream(StreamCapture* capture, int inputdeviceid, int sndgrpid, int samplerate, int channels, int framesize) override;
+        // input
+        inputstreamer_t NewStream(StreamCapture* capture,
+                                  int inputdeviceid, int sndgrpid,
+                                  int samplerate, int channels,
+                                  int framesize) override;
         bool StartStream(inputstreamer_t streamer) override;
+        bool StopStream(inputstreamer_t streamer) override;
         void CloseStream(inputstreamer_t streamer) override;
         bool IsStreamStopped(inputstreamer_t streamer) override;
         bool UpdateStreamCaptureFeatures(inputstreamer_t streamer) override;
 
-        // Output
-        outputstreamer_t NewStream(StreamPlayer* player, int outputdeviceid, int sndgrpid, int samplerate, int channels, int framesize) override;
+        // output
+        outputstreamer_t NewStream(StreamPlayer* player, int outputdeviceid,
+                                   int sndgrpid, int samplerate, int channels,
+                                   int framesize) override;
         void CloseStream(outputstreamer_t streamer) override;
+
         bool StartStream(outputstreamer_t streamer) override;
         bool StopStream(outputstreamer_t streamer) override;
         bool IsStreamStopped(outputstreamer_t streamer) override;
 
-        // Duplex 
-        duplexstreamer_t NewStream(StreamDuplex* duplex, int inputdeviceid, int outputdeviceid, int sndgrpid, int samplerate, int input_channels, int output_channels, int framesize) override { return nullptr; }
+        // duplex (بدون تغییر جهت حفظ تطابق با ساختار پایه)
+        duplexstreamer_t NewStream(StreamDuplex* duplex, int inputdeviceid,
+                                   int outputdeviceid, int sndgrpid,
+                                   int samplerate, int input_channels,
+                                   int output_channels, int framesize) override { return duplexstreamer_t(); }
         void CloseStream(duplexstreamer_t streamer) override { }
         bool StartStream(duplexstreamer_t streamer) override { return false; }
         bool IsStreamStopped(duplexstreamer_t streamer) override { return true; }
-        bool AddDuplexOutputStream(StreamDuplex* duplex, StreamPlayer* player) override { return false; }
-        bool RemoveDuplexOutputStream(StreamDuplex* duplex, StreamPlayer* player) override { return false; }
+        bool AddDuplexOutputStream(StreamDuplex* duplex,
+                                   StreamPlayer* player) override { return false; }
+        bool RemoveDuplexOutputStream(StreamDuplex* duplex,
+                                      StreamPlayer* player) override { return false; }
 
     public:
+
         virtual ~OboeWrapper();
         static std::shared_ptr<OboeWrapper> getInstance();
 
-        bool GetDefaultDevices(int& inputdeviceid, int& outputdeviceid) override;
-        bool GetDefaultDevices(SoundAPI sndsys, int& inputdeviceid, int& outputdeviceid) override;
+        bool GetDefaultDevices(int& inputdeviceid,
+                               int& outputdeviceid) override;
+        bool GetDefaultDevices(SoundAPI sndsys,
+                               int& inputdeviceid,
+                               int& outputdeviceid) override;
     };
 
     typedef SSB::soundgroup_t soundgroup_t;
     typedef SSB::inputstreamer_t inputstreamer_t;
     typedef SSB::outputstreamer_t outputstreamer_t;
+    typedef SSB::duplexstreamer_t duplexstreamer_t;
 
 }
 
