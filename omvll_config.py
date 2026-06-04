705e7fb86b3c26c8d251496cfe8184c67556050c
maanimaani4321 <maani.live@gmail.com>
2026-06-05 03:21:19 +0330

    test

--- omvll_config.py
+++ omvll_config.py
@@ -1,31 +1,30 @@
 import omvll
-from functools import lru_cache
 
 class MyConfig(omvll.ObfuscationConfig):
     def __init__(self):
         super().__init__()
 
-    def obfuscate_arithmetic(self, mod: omvll.Module, fun: omvll.Function):
-        return True
-
-    def flatten_cfg(self, mod: omvll.Module, func: omvll.Function):
-        return True
-
+    # رمزنگاری رشته‌ها (بسیار عالی برای مخفی کردن لایسنس و نام کلاس‌ها)
     def obfuscate_string(self, mod, func, string: bytes):
         return omvll.StringEncOptGlobal()
 
-    def indirect_call(self, mod: omvll.Module, func: omvll.Function):
-        return omvll.ObfuscationConfig.default_config(self, mod, func, [], [], [], 10)
+    # اسپاگتی کردن جریان کد (عالی برای از کار انداختن IDA Pro)
+    def flatten_cfg(self, mod, func):
+        return True
 
-    def break_control_flow(self, mod: omvll.Module, func: omvll.Function):
-        return omvll.ObfuscationConfig.default_config(self, mod, func, [], [], [], 10)
+    # پیچیده کردن محاسبات ریاضی
+    def obfuscate_arithmetic(self, mod, fun):
+        return True
 
-    def function_outline(self, _, __):
-        return omvll.FunctionOutlineWithProbability(10)
+    # این سه مورد زیر معمولاً باعث خطای Duplicate Symbol می‌شوند، پس غیرفعال می‌کنیم
+    def function_outline(self, mod, func):
+        return False 
 
-    def basic_block_duplicate(self, _, __):
-        return omvll.BasicBlockDuplicateWithProbability(10)
+    def basic_block_duplicate(self, mod, func):
+        return False
 
-@lru_cache(maxsize=1)
-def omvll_get_config():
+    def indirect_call(self, mod, func):
+        return False
+
+def omvll_get_config(module=None):
     return MyConfig()
