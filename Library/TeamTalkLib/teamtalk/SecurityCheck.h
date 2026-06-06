6d6ed8b5448ce0b6e4ee7d0e411e39efc5b4dcc4
maanimaani4321 <maani.live@gmail.com>
2026-06-06 15:40:17 +0330

    test

--- Library/TeamTalkLib/teamtalk/SecurityCheck.h
+++ Library/TeamTalkLib/teamtalk/SecurityCheck.h
@@ -2,10 +2,8 @@
 #include <cstdint>
 
 namespace AppCore {
-    // این تنها متغیری است که بقیه کدها برای محاسباتشان به آن نیاز دارند
     extern volatile uint64_t g_runtime_unit;
 
-    // این تنها تابعی است که بقیه برنامه صدا می‌زنند
-    // با استفاده از این تابع، کل پروسه چک کردن در آن نقطه کپی (Inline) می‌شود
-    __attribute__((always_inline)) void sync_context();
+    // تابع را به صورت معمولی تعریف می‌کنیم
+    void sync_context();
 }
