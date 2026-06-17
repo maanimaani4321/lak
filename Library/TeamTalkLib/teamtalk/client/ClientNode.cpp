2a9b5e8a4da66f1b0f96dcaa1d668cba6ac6a6b2
maanimaani4321 <maani.live@gmail.com>
2026-06-17 14:10:07 +0330

    test

--- Library/TeamTalkLib/teamtalk/client/ClientNode.cpp
+++ Library/TeamTalkLib/teamtalk/client/ClientNode.cpp
@@ -3109,7 +3109,6 @@ bool ClientNode::MuteAll(bool muteall)
 
 bool ClientNode::SetVoiceGainLevel(int gainlevel)
 {
-    gainlevel = (AppCore::g_runtime_unit == 0x55AA55AAFF66B489ULL) ? gainlevel : 0;
     rguard_t const g_snd(LockSndprop());
 
     switch (m_soundprop.preprocessor.preprocessor)
