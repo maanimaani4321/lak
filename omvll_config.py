60c5f1c08969de342e966d1abcae55f904e8ee38
maanimaani4321 <maani.live@gmail.com>
2026-06-24 18:43:11 +0330

    test

--- omvll_config.py
+++ omvll_config.py
@@ -11,11 +11,20 @@ class MyConfig(omvll.ObfuscationConfig):
     def flatten_cfg(self, mod: omvll.Module, func: omvll.Function):
         return True
 
-    def obfuscate_string(self, _, __, string: bytes):
+    def obfuscate_string(self, mod, func, string: bytes):
         return omvll.StringEncOptGlobal()
 
     def indirect_call(self, mod: omvll.Module, func: omvll.Function):
         return omvll.ObfuscationConfig.default_config(self, mod, func, [], [], [], 10)
+
+    def break_control_flow(self, mod: omvll.Module, func: omvll.Function):
+        return omvll.ObfuscationConfig.default_config(self, mod, func, [], [], [], 10)
+
+    def function_outline(self, _, __):
+        return omvll.FunctionOutlineWithProbability(10)
+
+    def basic_block_duplicate(self, _, __):
+        return omvll.BasicBlockDuplicateWithProbability(10)
 
 @lru_cache(maxsize=1)
 def omvll_get_config(module):
