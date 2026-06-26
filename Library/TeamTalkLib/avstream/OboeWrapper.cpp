412f7e0a60896bcb9f816ef4b1728c2231cd3744
maanimaani4321 <maani.live@gmail.com>
2026-06-26 23:58:02 +0330

    Test

--- Library/TeamTalkLib/avstream/OboeWrapper.cpp
+++ Library/TeamTalkLib/avstream/OboeWrapper.cpp
@@ -300,6 +300,9 @@ bool OboeWrapper::StartStream(inputstreamer_t streamer) {
 void OboeWrapper::CloseStream(inputstreamer_t streamer) {
     if (streamer && streamer->stream) {
         streamer->stream->requestStop();
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(75));
+        
         streamer->stream->close();
         streamer->stream.reset();
     }
@@ -516,6 +519,8 @@ outputstreamer_t OboeWrapper::NewStream(soundsystem::StreamPlayer* player, int o
 void OboeWrapper::CloseStream(outputstreamer_t streamer) {
     if (streamer && streamer->stream) {
         streamer->stream->requestStop();
+        std::this_thread::sleep_for(std::chrono::milliseconds(75));
+        
         streamer->stream->close();
         streamer->stream.reset();
         MYTRACE(ACE_TEXT("Closed Oboe playback stream\n"));
